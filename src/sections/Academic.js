6bb61306a5ae4375e7acc72da6289542457a9142
suryansh-sahay <dark1995monster@gmail.com>
2025-03-27 17:10:17 +0530

    updated

--- src/sections/Academic.js
+++ src/sections/Academic.js
@@ -4,12 +4,13 @@ import content from '../data/content.json';
 const About = () => {
     return (
         <section className="p-4 bg-gray-100 rounded-lg shadow-lg">  
-            <h2 className="text-2xl font-bold mb-2">My Acadmics</h2>
-            <p> {content.Academic}</p>
+            <h2 className="text-2xl font-bold mb-2">My Academics</h2>
+            <p><strong>Degree:</strong> {content.academic.degree}</p>
+            <p><strong>Major:</strong> {content.academic.major}</p>
+            <p><strong>University:</strong> {content.academic.university}</p>
+            <p><strong>Year:</strong> {content.academic.year}</p>
          </section>
-
     );
 };      
 
 export default About;
-
